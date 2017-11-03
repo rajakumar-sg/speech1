ef8cb43fcf8f0d3c89bcbb4995a4de70f9a00227
Raj@fourguyslab <torajakumar@gmail.com>
2017-11-03 23:29:18 +0800

    Speech to text - quick try

---
+++ src/main/java/com/hackathon/speech1/SpeechToText.java
@@ -0,0 +1,31 @@
+package com.hackathon.speech1;
+
+import edu.cmu.sphinx.api.Configuration;
+import edu.cmu.sphinx.api.SpeechResult;
+import edu.cmu.sphinx.api.StreamSpeechRecognizer;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class SpeechToText {
+    public static void main(String[] args) throws IOException {
+        Configuration configuration = new Configuration();
+
+        configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
+        configuration.setDictionaryPath("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict");
+        configuration.setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin");
+
+        StreamSpeechRecognizer recognizer = new StreamSpeechRecognizer(configuration);
+        InputStream stream = new FileInputStream(new File("man2_orig.wav"));
+
+        recognizer.startRecognition(stream);
+
+        SpeechResult result;
+        while ((result = recognizer.getResult()) != null) {
+            System.out.format("Hypothesis: %s\n", result.getHypothesis());
+        }
+        recognizer.stopRecognition();
+    }
+}
