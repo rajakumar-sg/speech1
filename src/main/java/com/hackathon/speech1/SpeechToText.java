9e6c05cfc4d169f617216c7bceaa44980083521d
Raj@fourguyslab <torajakumar@gmail.com>
2017-11-05 17:18:01 +0800

    Speech to text - additional wav files

--- src/main/java/com/hackathon/speech1/SpeechToText.java
+++ src/main/java/com/hackathon/speech1/SpeechToText.java
@@ -3,11 +3,15 @@ package com.hackathon.speech1;
 import edu.cmu.sphinx.api.Configuration;
 import edu.cmu.sphinx.api.SpeechResult;
 import edu.cmu.sphinx.api.StreamSpeechRecognizer;
+import edu.cmu.sphinx.linguist.dictionary.Word;
+import edu.cmu.sphinx.result.WordResult;
+import edu.cmu.sphinx.util.TimeFrame;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 public class SpeechToText {
     public static void main(String[] args) throws IOException {
@@ -26,9 +30,23 @@ public class SpeechToText {
         recognizer.startRecognition(stream);
 
         SpeechResult result;
-        while ((result = recognizer.getResult()) != null) {
+        /*while ((result = recognizer.getResult()) != null) {
             System.out.format("Hypothesis: %s\n", result.getHypothesis());
+        }*/
+
+        while ((result = recognizer.getResult()) != null) {
+            //System.out.format("Hypothesis: %s\n", result.getHypothesis());
+            for (WordResult wordResult : result.getWords()) {
+                TimeFrame timeFrame = wordResult.getTimeFrame();
+                Word word = wordResult.getWord();
+                System.out.println("Start :" + timeFrame.getStart() +
+                        ", End :" + timeFrame.getEnd() +
+                        ", Duration :" + timeFrame.length() +
+                        ", Word :" + word.getSpelling());
+
+            }
         }
+
         recognizer.stopRecognition();
     }
 }
